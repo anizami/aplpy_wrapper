e65bf03da49770611be274f1bce32b508428139d
Asra Nizami <anizami@macalester.edu>
2014-07-02 17:18:59 +0500

    finished ticks class except for one bug

--- aplpy_wrapper/ticks.py
+++ aplpy_wrapper/ticks.py
@@ -1,4 +1,3 @@
-from wcsaxes.ticks import Ticks
 import astropy.units as u
 
 
@@ -6,54 +5,55 @@ class APLpyTicks(object):
 
     def __init__(self, axes):
         self._ax = axes
+        self.x_visible_axes = None
+        self.y_visible_axes = None
 
     def set_xspacing(self, spacing):
+        '''
+        Set the x-axis tick spacing, in degrees.
+        '''
         #  TODO: Assumes coords indices are 0 and 1 - not true for cube data
         self._ax.coords[0].set_ticks(spacing=spacing * u.degree)
 
     def set_yspacing(self, spacing):
+        '''
+        Set the y-axis tick spacing, in degrees.
+        '''
         #  TODO: Assumes coords indices are 0 and 1 - not true for cube data
         self._ax.coords[1].set_ticks(spacing=spacing * u.degree)
 
     def set_color(self, color):
-        # Can only set this in WCSAxes through set_ticks(values, spacing, color=...)
-        pass
+        self._ax.coords[0].set_ticks(color=color)
+        self._ax.coords[1].set_ticks(color=color)
 
     def set_length(self, length, minor_factor=0.5):
-        #  Minor factor is irrelevant for WCSAxes
-        # Can only set this in WCSAxes through set_ticks(values, spacing, width=...)        
-        # self._ax.coords[0].set_ticksize(length)
-        # self._ax.coords[1].set_ticksize(length)
-        pass
+        self._ax.coords[0].set_ticks(size=length)
+        self._ax.coords[1].set_ticks(size=length)
 
     def set_linewidth(self, linewidth):
-        # Can only set this in WCSAxes through set_ticks(values, spacing, size=...)
-        pass
+        self._ax.coords[0].set_ticks(width=linewidth)
+        self._ax.coords[1].set_ticks(width=linewidth)
 
     def set_minor_frequency(self, frequency):
         #  WCSAxes doesn't have minor ticks
         pass
 
     def show(self):
-        self._ax.coords[0].set_visible_axes('all')
-        self._ax.coords[1].set_visible_axes('all')
-        pass
+        self._ax.coords[0].set_ticks_position('all')
+        self._ax.coords[1].set_ticks_position('all')
 
     def hide(self):
-        #  WCSAxes doesn't allow removing from _visible_axes
-        pass
+        self._ax.coords[0].set_ticks_position('')
+        self._ax.coords[1].set_ticks_position('')
 
     def show_x(self):
-        self._ax.coords[0].set_visible_axes('all')
+        self._ax.coords[0].set_ticks_position('all')
 
     def hide_x(self):
-        #  WCSAxes doesn't allow removing from _visible_axes
-        pass
+        self._ax.coords[0].set_ticks_position('')
 
     def show_y(self):
-        self._ax.coords[1].set_visible_axes('all')
+        self._ax.coords[1].set_ticks_position('all')
 
     def hide_y(self):
-        #  WCSAxes doesn't allow removing from _visible_axes
-        pass
-
+        self._ax.coords[1].set_ticks_position('')
